40a240c70dd4d5236c7f44a675cd23b9e3cb0297
bennykoval <bethanykoval@gmail.com>
2019-07-11 12:52:41 -0400

    Add script, stylesheet, and form

---
+++ main.js
@@ -0,0 +1,6 @@
+const searchField = document.querySelector('#search-field')
+const searchButton = document.querySelector('#search-button')
+
+// searchButton.addEventListener('click', function () {
+
+// }
