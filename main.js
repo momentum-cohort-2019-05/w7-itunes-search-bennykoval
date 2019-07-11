1188665dc64a40e85d6836741140532f40d7ee02
bennykoval <bethanykoval@gmail.com>
2019-07-11 15:22:28 -0400

    Retrieve data on form submit, specify search

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const searchField = document.querySelector('#search-field')
 const searchButton = document.querySelector('#search-button')
+const searchForm = document.querySelector('#search-form')
 
-// searchButton.addEventListener('click', function () {
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let newUrl = makeNewUrl();
+    fetch(newUrl)
+        .then(response => response.json())
+
+        // .then(data => {displayData(data)})
+        .then(data => {console.log(data)})
+})
+
+function makeNewUrl() {
+    const userInput = searchField.value;
+    let uriConverted = encodeURIComponent(userInput);
+    let term = `term=${uriConverted}`;
+    let attribute = 'attribute=artistTerm';
+    let media = 'media=music';
+    let entity = 'entity=song';
+
+    return `https://itunes-api-proxy.glitch.me/search?${term}&${media}&${entity}&${attribute}`;
+}
+
+// function displayData() {
 
 // }
