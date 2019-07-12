df450f7751fe2c1ef047f7a2ff3d951cc234fb02
bennykoval <bethanykoval@gmail.com>
2019-07-11 21:15:24 -0400

    Audio player all set

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-const searchField = document.querySelector('#search-field')
-const searchButton = document.querySelector('#search-button')
+// const searchButton = document.querySelector('#search-button')
 const searchForm = document.querySelector('#search-form')
+const musicPlayer = document.querySelector('#audio-player')
 
-searchForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let newUrl = makeNewUrl();
-    fetch(newUrl)
-        .then(response => response.json())
 
-        // .then(data => {displayData(data)})
-        .then(data => {console.log(data)})
-})
+function musicNode (music) {
+    const musicDiv = document.createElement('div');
+    musicDiv.classList.add('music');
+    musicDiv.innerHTML = `
+        <ul class="detail-list">
+            <li class="list-item img"><img id="img-clicky" data-preview="${music.previewUrl}" src=${music.artworkUrl100}></li>
+            <li class="list-item title">${music.trackName}</li>
+            <li class="list-item artist">${music.artistName}</li>
+        </ul>
+    `
+    return musicDiv;
+}
 
 function makeNewUrl() {
+    const searchField = document.querySelector('#search-field')
     const userInput = searchField.value;
     let uriConverted = encodeURIComponent(userInput);
     let term = `term=${uriConverted}`;
@@ -23,6 +28,28 @@ function makeNewUrl() {
     return `https://itunes-api-proxy.glitch.me/search?${term}&${media}&${entity}&${attribute}`;
 }
 
-// function displayData() {
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-// }
+    const resultsDiv = document.querySelector('#search-results');
+
+    let newUrl = makeNewUrl();
+    fetch(newUrl)
+        .then(response => response.json())
+
+        // .then(data => {musicNode(data)})
+        // .then(data => {console.log(data)})
+        .then(function (data) {
+            resultsDiv.innerHTML = ''
+            for (let music of data.results) {
+              resultsDiv.appendChild(musicNode(music))
+            }
+        })
+})
+
+document.querySelector('#search-results').addEventListener('click', function (event) {
+    if (event.target && event.target.matches('#img-clicky')) {
+        musicPlayer.src = event.target.dataset.preview;
+        musicPlayer.autoplay = "true";
+    }
+})
