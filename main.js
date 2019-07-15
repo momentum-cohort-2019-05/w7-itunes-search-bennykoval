04131a155c805a27be32e00dcf9b7b715d03b640
bennykoval <bethanykoval@gmail.com>
2019-07-14 20:06:06 -0400

    Functional progress bar

--- main.js
+++ main.js
@@ -2,54 +2,170 @@
 const searchForm = document.querySelector('#search-form')
 const musicPlayer = document.querySelector('#audio-player')
 
-
-function musicNode (music) {
+function musicNode(music) {
     const musicDiv = document.createElement('div');
     musicDiv.classList.add('music');
+    if (music.trackName.length >= 20) {
+        music.trackName = music.trackName.substring(0, 20) + "..."
+    }
+
+    if (music.artistName.length >= 20) {
+        music.artistName = music.artistName.substring(0, 20) + "..."
+    }
+
     musicDiv.innerHTML = `
         <ul class="detail-list">
-            <li class="list-item img"><img id="img-clicky" data-preview="${music.previewUrl}" src=${music.artworkUrl100}></li>
-            <li class="list-item title">${music.trackName}</li>
-            <li class="list-item artist">${music.artistName}</li>
+            <li class="list-item" id="img"><img id="img-clicky" data-preview="${music.previewUrl}" src=${music.artworkUrl100}></li>
+            <li class="list-item" id="title">${music.trackName}</li>
+            <li class="list-item" id="artist">${music.artistName}</li>
         </ul>
     `
     return musicDiv;
 }
 
-function makeNewUrl() {
+function artistNode(music) {
+    const artistDiv = document.createElement('div');
+    console.log(music)
+    artistDiv.classList.add('music');
+    artistDiv.innerHTML = `
+        <ul class="detail-list">
+            <li class="list-item" id="artist">${music.artistName}</li>
+        </ul>
+    `
+    return artistDiv;
+}
+
+function albumNode(music) {
+    const albumDiv = document.createElement('div');
+    albumDiv.classList.add('music');
+    if (music.collectionName.length >= 20) {
+        music.collectionName = music.collectionName.substring(0, 20) + "..."
+    }
+
+    if (music.artistName.length >= 20) {
+        music.artistName = music.artistName.substring(0, 20) + "..."
+    }
+    albumDiv.innerHTML = `
+        <ul class="detail-list">
+            <li class="list-item" id="img"><img id="img-clicky" data-preview="${music.previewUrl}" src=${music.artworkUrl100}></li>
+            <li class="list-item" id="title">${music.collectionName}</li>
+            <li class="list-item" id="artist">${music.artistName}</li>
+        </ul>
+    `
+    return albumDiv;
+}
+function makeNewUrl(entity) {
     const searchField = document.querySelector('#search-field')
     const userInput = searchField.value;
     let uriConverted = encodeURIComponent(userInput);
     let term = `term=${uriConverted}`;
-    let attribute = 'attribute=artistTerm';
+    // let attribute = 'attribute=artistTerm';
     let media = 'media=music';
-    let entity = 'entity=song';
+    // let entity = 'entity=song';
 
-    return `https://itunes-api-proxy.glitch.me/search?${term}&${media}&${entity}&${attribute}`;
+    return `https://itunes-api-proxy.glitch.me/search?${term}&${media}&entity=${entity}`;
 }
 
 searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const resultsDiv = document.querySelector('#search-results');
+    const songDiv = document.querySelector('#song-search-results');
+    const artistDiv = document.querySelector('#artist-search-results');
+    const albumDiv = document.querySelector('#album-search-results')
+    const showOnSearch = document.querySelector('#show-on-search')
 
-    let newUrl = makeNewUrl();
+    let newUrl = makeNewUrl('song');
     fetch(newUrl)
         .then(response => response.json())
 
-        // .then(data => {musicNode(data)})
-        // .then(data => {console.log(data)})
         .then(function (data) {
-            resultsDiv.innerHTML = ''
+            showOnSearch.style.display = 'block'
+            songDiv.innerHTML = ''
+            data.results.length = Math.min(data.results.length, 3)
             for (let music of data.results) {
-              resultsDiv.appendChild(musicNode(music))
-            }
+                    songDiv.appendChild(musicNode(music))
+                }
         })
+
+
+    newUrl = makeNewUrl('musicArtist');
+    fetch(newUrl)
+        .then(response => response.json())
+
+        .then(function (data) {
+            showOnSearch.style.display = 'block'
+            artistDiv.innerHTML = ''
+            data.results.length = Math.min(data.results.length, 3)
+            for (let music of data.results) {
+                    artistDiv.appendChild(artistNode(music))
+                }
+        })
+
+    
+    newUrl = makeNewUrl('album');
+    fetch(newUrl)
+        .then(response => response.json())
+
+        .then(function (data) {
+            showOnSearch.style.display = 'block'
+            albumDiv.innerHTML = ''
+            data.results.length = Math.min(data.results.length, 3)
+            for (let music of data.results) {
+                    albumDiv.appendChild(albumNode(music))
+                }
+
 })
 
-document.querySelector('#search-results').addEventListener('click', function (event) {
+document.querySelector('.search-results').addEventListener('click', function (event) {
     if (event.target && event.target.matches('#img-clicky')) {
         musicPlayer.src = event.target.dataset.preview;
         musicPlayer.autoplay = "true";
     }
+  })
 })
+
+function calculateTotalValue(length) {
+    let minutes = Math.floor(length / 60),
+      secondsInt = length - minutes * 60,
+      secondsStr = secondsInt.toString(),
+      seconds = secondsStr.substr(0, 2),
+      time = minutes + ':' + seconds
+  
+    return time;
+  }
+
+function calculateCurrentValue(currentTime) {
+let currentHour = parseInt(currentTime / 3600) % 24,
+    currentMinute = parseInt(currentTime / 60) % 60,
+    secondsLong = currentTime % 60,
+    currentSeconds = secondsLong.toFixed(),
+    current_time = (currentMinute < 10 ? "0" + currentMinute : currentMinute) + ":" + (currentSeconds < 10 ? "0" + currentSeconds : currentSeconds);
+
+return current_time;
+}
+
+
+function functProgressBar() {
+    let player = document.querySelector('#audio-player');
+    let length = player.duration;
+    let current_time = player.currentTime;
+  
+    let totalLength = calculateTotalValue(length);
+    jQuery("#end-time").html(totalLength);
+    
+    let currentTime = calculateCurrentValue(current_time);
+    jQuery("#start-time").html(currentTime);
+
+    let progressBar = document.querySelector('#progress-bar');
+    progressBar.value = (player.currentTime / player.duration);
+    progressBar.addEventListener("click", seek);
+
+
+    function seek(evt) {
+        let percent = evt.offsetX / this.offsetWidth;
+        player.currentTime = percent * player.duration;
+        progressBar.value = percent / 100;
+    }
+};
+
+// functProgressBar(jQuery('#audio-player-container').length);
